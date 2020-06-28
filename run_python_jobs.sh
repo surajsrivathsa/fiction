755604d33f5c2614b35024a2578717ab90f82147
chandan126 <chandanbhat1265@gmail.com>
2020-06-28 14:52:05 +0200

    Updated the batch and shell file

--- run_python_jobs.sh
+++ run_python_jobs.sh
@@ -3,7 +3,7 @@
 #CREATED ON : MAY-2020
 #DESCRIPTION: SHELL SCRIPT TO RUN PYTHON JOBS IN BACKGROUND 
 #ACTIVATE THE REQUIRED ENVIRONMENT, RUN THE PYTHON PROGRAM BY PASSING RELEVANT PARAMETERS
-#PARAMETERS: PARAMETERS: $1 - ENVIRONMENT_NAME  , $2 - FEATURE_FILE_PATH, $3 - BOOK_FILE_PATH,$4 - LANGUAGE, $5 - $NEW_FEATURE_FILE_PATH
+#PARAMETERS: $1 - ENVIRONMENT_NAME  , $2 - FEATURE_FILE_PATH, $3 - BOOK_FILE_PATH,$4 - LANGUAGE, $5 - $NEW_FEATURE_FILE_PATH $6 - PYTHON ENVIRONMENT
 ########################################################################
 
 
@@ -14,7 +14,8 @@ BOOK_FILE_PATH=$2;
 LANGUAGE=$3;
 ENCODING=$4;
 NEW_FEATURE_FILE_PATH=$5;
-
+MY_ENVIRON=$6
+MY_PYTHON_PATH=$7
 
 echo "Printing the parameters passed to shell"
 for i in $*; do 
@@ -23,11 +24,11 @@ done
 
 
 echo ""
-echo "Activating python environment: $ENVIRONMENT_NAME"
-source activate $ENVIRONMENT_NAME
+echo "Activating python environment: $MY_ENVIRON"
+source activate $MY_ENVIRON
 
 echo "Running python program driver file"
-python /Users/Chandan/git/fiction/Feature1/feature1_driver.py --feature_file_path $FEATURE_FILE_PATH --book_file_path $BOOK_FILE_PATH --emoticon_file_path  --encoding $ENCODING --new_feature_file_path $NEW_FEATURE_FILE_PATH
+python $MY_PYTHON_PATH --feature_file_path $FEATURE_FILE_PATH --book_file_path $BOOK_FILE_PATH --emoticon_file_path  --encoding $ENCODING --new_feature_file_path $NEW_FEATURE_FILE_PATH
 
 status=$?
 
